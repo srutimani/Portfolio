2bb1c0813d58815859a966e20e07c8baf3efb40d
srutimani <57199080+srutimani@users.noreply.github.com>
2020-04-16 15:00:52 -0700

    Added all files from replit portfolio

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links [Srutilaya Mani Period: 7]</p>'; ?> 
+    <ul>
+      <li><a href="index.html">index.html</a></li></br>
+      
+      <li><a href="Mani_AboutMe.html">AboutMe.html</a></li></br>
+      <li><a href="Mani_Projects.html">Projects.html</a></li></br>
+      <li><a href="Mani_Resume.html">Resume.html</a></li></br>
+      <li><a href="Mani_ContactMe.html">ContactMe.html</a></li></br>
+    <ul>
+  </body>
+</html>
